a1022a2e5b656e60306ecb2ec37f74997f1cd6f6
root <root@ibm-p8-kvm-03-guest-02.virt.pnr.lab.eng.rdu2.redhat.com>
2022-06-14 09:09:40 +0300

    add test parser

---
+++ lib/fortune_elixir/book_parser.ex
@@ -0,0 +1,35 @@
+defmodule FortuneElixir.BookParser do
+  @str_limit 50
+  @page_limit 30
+
+  # {pages_count, result} = FortuneElixir.BookParser.test
+  def test do
+    "priv/books/pratchett_moris.txt"
+    |> File.stream!()
+    |> Stream.map(&String.split/1)
+    |> Enum.concat
+    |> make_pages
+  end
+
+  # second param:
+  #   {letters_in_row_counter::int, rows_in_page_counter::int, rows_sum::int,
+  #    row::string, page::map(), result_struct::map()}
+  def make_pages(words), do: make_pages(words, {0, 0, 0, "", %{}, %{}})
+  def make_pages([], {_, rows_in_page, sum, row, page, result}) do
+    {sum + 1, Map.put(result, sum + 1, Map.put(page, rows_in_page + 1, row))}
+  end
+  def make_pages([word | rest_words] = words, {letters_in_row, rows_in_page, sum, row, page, result}) do
+    cond do
+      rows_in_page == @page_limit ->
+        new_sum = sum + 1
+        make_pages(words, {letters_in_row, 0, new_sum, row, %{}, Map.put(result, new_sum, page)})
+      letters_in_row + String.length(word) + 1 > @str_limit ->
+        new_rows_in_page = rows_in_page + 1
+        make_pages(words, {0, new_rows_in_page, sum, "", Map.put(page, new_rows_in_page, row), result})
+      letters_in_row == 0 ->
+        make_pages(rest_words, {letters_in_row + String.length(word), rows_in_page, sum, word, page, result})
+      true ->
+        make_pages(rest_words, {letters_in_row + String.length(word) + 1, rows_in_page, sum, row <> " " <> word, page, result})
+    end
+  end
+end
